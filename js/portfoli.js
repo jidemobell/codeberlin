1eb47a6addf3debccfc8c0cb3d1c3c127f279c45
jide mobell <jidd32@gmail.com>
2017-07-04 07:02:42 +0100

    committing all here

---
+++ js/portfoli.js
@@ -0,0 +1,25 @@
+$(document).ready(
+    function() {
+        $(".fa-top").hover(function() {
+            $(this).addClass("bounce");
+
+        }, function() {
+            $(this).removeClass("bounce");
+        });
+
+
+        $(".imgborder").hover(function() {
+            $(this).addClass("shake");
+
+        }, function() {
+            $(this).removeClass("shake");
+        });
+
+        $(".to-portfolio").click()
+
+
+
+
+
+    }
+);
